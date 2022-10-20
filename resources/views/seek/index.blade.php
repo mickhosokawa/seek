a83a220d7fc0a1402954a87c3a8fa6d9227dcb6e
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-10-20 15:32:58 +0900

    csrf不要のため削除

--- resources/views/seek/index.blade.php
+++ resources/views/seek/index.blade.php
@@ -54,7 +54,6 @@
     {{-- 検索 --}}
   <div class="flex flex-row bg-blue-800">
     <form action="{{route('jobs')}}" method="get">
-        @csrf
         <div class="flex justify-center max-w-screen-md m-5 w-11/12">
             <div>
                 <p class="text-white text-bold text-lg ml-5">What</p>
