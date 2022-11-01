355b7107c08c0387fd3d3d907b1bbefb8a70d6dc
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-11-01 22:39:05 +0900

    検索フォーム、ナビゲーションを修正

--- resources/views/admin/companies/index.blade.php
+++ resources/views/admin/companies/index.blade.php
@@ -9,8 +9,7 @@
         <div class="container px-5 py-24 mx-auto">
           {{-- 企業検索フォーム --}}
             <div class="flex flex-col text-center w-full mb-12">
-              <form action="{{ route('admin.companies.search') }}" method="POST">
-                @csrf
+              <form action="{{ route('admin.companies.search') }}" method="GET">
               <div>
                 <input type="text" name="keywords" id="keywords" placeholder="Enter company's name" value=
                 "@if (isset($keywords)){{ $keywords }}
@@ -41,7 +40,7 @@
                   <td class="md:px-4 py-3">{{$company->email}}</td>
                   <td class="md:px-4 py-3">{{$company->created_at}}</td>
                   <td class="md:px-4 py-3">
-                    <button onclick="location.href='#'" class="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded ">Edit</button>
+                    <button onclick="location.href='{{ route('admin.companies.edit', $company->id)}}'" class="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded ">Edit</button>
                   </td>
                   <td class="md:px-4 py-3">
                     <button onclick="location.href='#'" class="text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded ">Delete</button>
