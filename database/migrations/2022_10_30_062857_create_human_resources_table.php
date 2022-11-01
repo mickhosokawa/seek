8dd5934499ab41c664698c9b13d93c845c21febd
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-11-01 22:38:33 +0900

    人事テーブル更新

--- database/migrations/2022_10_30_062857_create_human_resources_table.php
+++ database/migrations/2022_10_30_062857_create_human_resources_table.php
@@ -15,12 +15,32 @@ return new class extends Migration
     public function up()
     {
         Schema::create('human_resources', function (Blueprint $table) {
+            // $table->id();
+            // $table->string('name')->nullable();
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->foreignId('companies_id')->constrained();
+            // $table->timestamps();
+            // $table->softDeletes();
             $table->id();
             $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId('companies_id')->constrained();
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('url')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('background_image')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('company_size')->nullable();
+            $table->text('specialities')->nullable();
+            $table->text('our_mission_statement')->nullable();
+            $table->text('featured')->nullable();
+            $table->text('other')->nullable();
+            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
