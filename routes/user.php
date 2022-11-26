0fb2a236666d8becc34a6d7e37a748044ce21774
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-11-26 16:15:31 +0900

    Route設定 #10

---
+++ routes/user.php
@@ -0,0 +1,68 @@
+<?php
+
+use App\Http\Controllers\Company\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Company\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Company\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Company\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Company\Auth\NewPasswordController;
+use App\Http\Controllers\Company\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Company\Auth\RegisteredUserController;
+use App\Http\Controllers\Company\Auth\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\User\ProfileController;
+
+Route::get('/dashboard', function () {
+    return view('user.dashboard');
+})->middleware(['auth:companies', 'verified'])->name('dashboard');
+
+Route::middleware('guest')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+                ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+                ->name('password.request');
+
+    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+                ->name('password.email');
+
+    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+                ->name('password.reset');
+
+    Route::post('reset-password', [NewPasswordController::class, 'store'])
+                ->name('password.update');
+});
+
+Route::middleware('auth:companies')->group(function () {
+    Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
+                ->name('verification.notice');
+
+    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+                ->middleware(['signed', 'throttle:6,1'])
+                ->name('verification.verify');
+
+    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+                ->middleware('throttle:6,1')
+                ->name('verification.send');
+
+    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+                ->name('password.confirm');
+
+    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->name('logout');
+    
+    // プロフィール関連
+    Route::get('profile/me{id}', [ProfileController::class, 'show'])->name('profile.show');
+    Route::post('profile/me/{id}', [ProfileController::class, 'store'])->name('profile.create');
+    Route::get('profile/me/{id}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('profile/me/{id}', [ProfileController::class, 'update'])->name('profile.update');
+});
