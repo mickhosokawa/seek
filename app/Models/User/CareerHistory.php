ed796778411eb69f689a4507971d88c9b81495cb
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-11-26 17:21:37 +0900

    経歴テーブルの追加 #10

---
+++ app/Models/User/CareerHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CareerHistory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'role',
+        'job_title',
+        'company_name',
+        'started_year',
+        'started_month',
+        'ended_year',
+        'ended_month',
+        'description',
+    ];
+
+}
