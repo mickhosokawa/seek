958bcabac8ad8d1e12424a40a23ba9a46ab9e8c1
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2022-11-26 17:55:12 +0900

    資格テーブルの追加 #10

---
+++ app/Models/LicenceOrCertification.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LicenceOrCertification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'licence_name',
+        'issuing_organization',
+        'issue_year',
+        'issue_month',
+        'expiry_year',
+        'expiry_month',
+        'is_expired',
+        'description',
+    ];
+}
