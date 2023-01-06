384bd34a348120fa24615d416d130bd252781a29
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-01-06 10:32:25 +0900

    ルーティング設定変更

--- routes/auth.php
+++ routes/auth.php
@@ -10,13 +10,6 @@ use App\Http\Controllers\User\Auth\RegisteredUserController;
 use App\Http\Controllers\User\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\User\ProfileController;
-use App\http\Controllers\User\ApplyJobController;
-use App\http\Controllers\User\SavedJobsController;
-use App\http\Controllers\User\SavedSearchesController;
-use App\http\Controllers\User\AppliedJobsController;
-use App\http\Controllers\User\ReviewJobsController;
-
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
@@ -60,36 +53,4 @@ Route::middleware('auth:users')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
-
-    // プロフィール登録関連
-    Route::get('profile/me/create', [ProfileController::class, 'create'])->name('profile.create');
-    Route::post('profile/me/create', [ProfileController::class, 'storePersonalDetail'])->name('profile.store.personal');
-    Route::get('profile/me/create/career', [ProfileController::class, 'createCareer'])->name('profile.career.create');
-    Route::post('profile/me/create/career', [ProfileController::class, 'storeCareer'])->name('profile.career.store');
-    Route::post('profile/me/{id}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-
-    // 求人応募関連
-    Route::get('jobs/detail/{id}/apply-a-job', [ApplyJobController::class, 'create'])->name('apply.job.create');
-    Route::post('jobs/detail/{id}/apply-a-job', [ApplyJobController::class, 'store'])->name('apply.job.store');
-
-    // お気に入り保存求人関連 ※後ほど実装
-    Route::get('my-activity/saved-jobs', [SavedJobsController::class, 'index'])->name('saved.jobs.index');
-    Route::get('my-activity/saved-jobs/{id}', [SavedJobsController::class, 'show'])->name('saved.jobs.show');
-    Route::post('my-activity/saved-jobs/{id}/destroy', [SavedJobsController::class, 'destroy'])->name('saved.jobs.destroy');
-    Route::post('my-activity/saved-jobs', [SavedJobsController::class, 'store'])->name('saved.jobs.store');
-
-    // お気に入り検索条件関連 ※後ほど実装
-    Route::get('my-activity/saved-searches', [SavedSearchesController::class, 'index'])->name('saved.searaches.index');
-    Route::get('my-activity/saved-searches', [SavedSearchesController::class, 'search'])->name('saved.searaches.search');
-    Route::post('my-activity/saved-searches', [SavedSearchesController::class, 'store'])->name('saved.searaches.store');
-    Route::post('my-activity/saved-searches/{id}/destroy', [SavedSearchesController::class, 'destroy'])->name('saved.searaches.destroy');
-
-    // 応募済み求人関連
-    Route::get('my-activity/applied-jobs', [AppliedJobsController::class, 'index'])->name('applied.jobs.index');
-    Route::get('my-activity/applied-jobs/{id}/show', [AppliedJobsController::class, 'show'])->name('applied.jobs.show');
-    Route::post('my-activity/applied/{id}/destroy', [AppliedJobsController::class, 'destroy'])->name('applied.jobs.destroy');
-
-    // レビュー投稿関連
-    Route::get('leave-a-review', [ReviewJobsController::class, 'create'])->name('leave.review.create');
-    Route::post('leave-a-review', [ReviewJobsController::class, 'store'])->name('leave.review.store');
 });
