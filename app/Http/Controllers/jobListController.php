8d94b774578ac0b7c5d78fe36f387db5b0bec901
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-01-23 08:51:50 +0900

    第二職種、サバーブ、州の取得を行い、求人情報一覧を閲覧できるようにした

--- app/Http/Controllers/jobListController.php
+++ app/Http/Controllers/jobListController.php
@@ -11,56 +11,68 @@ use App\Models\SubClassification;
 
 class JobListController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $classifications = Classification::all();
-        $jobOffers = JobOffer::with('SubClassification')->get()->toArray();
+        //$sort         = $request->get('sort');
+        //$jobs         = DB::table('jobs');
+        $jobOffers = JobOffer::with(['SubClassification', 'suburb.state'])->get();
 
-        return view('seek.index', compact('classifications','jobOffers', 'locations'));
+        // 入力された内容を取得
+        $search = $request->input('search');
+        $subClassification = $request->input('sub_classification');
+        //dd($search, $subClassification, $request);
+
+        // keywordが入力されていれば、AND検索を実行
+        // if($search){
+            $spaceConvert = mb_convert_kana($search, 's');
+            $searchKeyWords = preg_split('/[\s,]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY);
+
+            foreach($searchKeyWords as $searchKeyWord){
+                $jobOffers = JobOffer::where('title', 'LIKE', "%{$searchKeyWord}%")
+                                        //->where('sub_classification_id','=',$subClassification)
+                                        ->with(['suburb'])
+                                        ->get();
+                                        //->toArray();
+            }
+            //dd($jobOffers);
+        // }
+        //dd($search, $searchKeyWords, $jobOffers);
+
+        
+        // 並び替え
+        // if(!empty($sort)){
+        //     if($sort === 'relevance'){
+        //         $query->orderBy('jobs.id', 'asc');
+        //     }else{
+        //         $query->orderBy('jobs.created_at', 'desc');
+        //     }
+        // }else{
+        //     $query->orderBy('jobs.id', 'asc');
+        // }
+
+        // カテゴリ検索
+        if($subClassification){
+            // $jobOffers = 
+            // $jobOffers = JobOffer::where('sub_classification_id','=',$subClassification)
+            //                         ->where('title', 'LIKE', "%{$searchKeyWords}%")
+            //                         ->with('SubClassification')
+            //                         ->get()
+            //                         ->toArray();
+        }
+        
+
+        return view('seek.index', compact('classifications','jobOffers', 'search'));
 
     }
 
     public function search(Request $request)
     {
-        $classifications = Classification::all();
-        $sort         = $request->get('sort');
-        $jobs         = DB::table('jobs');
+        //$classifications = Classification::all();
         
-        // 入力された内容を取得
-        $search = $request->get('search');
-        $query = $jobs
-                ->leftJoin('classifications', 'jobs.job_id', '=', 'classifications.id');
 
-        // keywordが入力されていれば、AND検索を実行
-        if($search){
-            $spaceConvert = mb_convert_kana($search, 's');
-            $searchKeyWords = preg_split('/[\s,]+/', $spaceConvert, -1, PREG_SPLIT_NO_EMPTY);
-
-            foreach($searchKeyWords as $searchKeyWord){
-                $query->where('position', 'LIKE', "%{$searchKeyWord}%");
-            }
-        }
-        
-        // 並び替え
-        if(!empty($sort)){
-            if($sort === 'relevance'){
-                $query->orderBy('jobs.id', 'asc');
-            }else{
-                $query->orderBy('jobs.created_at', 'desc');
-            }
-        }else{
-            $query->orderBy('jobs.id', 'asc');
-        }
-
-        // カテゴリ検索
-        if(isset($classifications)){
-            $query->where('classifications.id','=',$request->input('id'));
-        }
-        //dd($request->input('classifications'));
-        
-        $jobs = $query->get();
     
-        return view('seek.jobs', compact('jobs', 'search', 'classifications'));
+        //return view('seek.jobs', compact('jobs', 'search', 'classifications'));
     }
 
     public function show($id)
--- resources/views/seek/index.blade.php
+++ resources/views/seek/index.blade.php
@@ -1,23 +1,23 @@
 <x-navigation-and-search>
 
 {{-- 検索欄 --}}
-<div class="flex flex-row bg-blue-800">
-  <form action="{{route('user.seek.list')}}" method="get">
-    <div class="flex justify-center max-w-screen-md m-5 w-11/12">
+<div class="flex flex-row justify-center w-full bg-blue-800">
+  <form action="{{route('user.seek.index')}}" method="get">
+    <div class="flex max-w-screen-md m-5 w-full">
       <div>
         <p class="text-white text-bold text-lg ml-5">What</p>
-        <input class="border rounded-md ml-5 mt-1" placeholder="Enter Keywords" type="search" name="search" value=
+        <input class="border border-slate-300 rounded-md ml-5 mt-1" placeholder="Search for anything..." type="search" name="search" value=
           "@if (isset($search)) {{ $search }} @endif">
       </div>
 
       {{-- 職種選択 --}}
       <div class="mt-7">
-        <select name="classifications" class="border rounded-md ml-5 mt-1" >
-          <option value="0" @if(\Request::input('classifications') === "0") selected @endif>All</option>
+        <select name="sub_classification" class="border rounded-md ml-5 mt-1" >
+          <option value="0" @if(\Request::input('sub_classification') === "0") selected @endif>All</option>
           @foreach ($classifications as $classification)
           <optgroup label="{{ $classification->name }}">
             @foreach ($classification->subClassification as $sub_classification)
-                <option value="{{ $sub_classification->id }}" @if(\Request::input('classifications') == $sub_classification->id) selected @endif>{{ $sub_classification->name }}</option>
+                <option value="{{ $sub_classification->id }}" @if(\Request::input('sub_classification') == $sub_classification->id) selected @endif>{{ $sub_classification->name }}</option>
             @endforeach
           </optgroup>
           @endforeach
@@ -30,6 +30,30 @@
           <input class="border rounded-md ml-5 mt-1" type="text" name="location">
       </div>
       <input type="submit" class="ml-2 px-6 py-1 mt-9 rounded bg-pink-600 text-white font-bold link-hover cursor-pointer" value="SEEK" selected>
+    </div>
+  </form>
+</div>
+
+    {{-- 投稿一覧 --}}
+    <div class="bg-gray-100 mt-3 justify-center">
+      <div class="bg-white max-w-screen-md m-auto rounded-md mt-1 mb-1 p-3 w-11/12">
+        @if (isset($jobOffers))
+          @foreach ($jobOffers as $job)
+          {{-- @dump($job); --}}
+          <h3 class="mt-5">
+            <a href="{{route('user.seek.show', ['id'=>$job->id]) }}" class="text-blue-600 text-2xl link-hover cursor-pointer">{{$job->title}}</a>
+            <p class="mt-3">{{$job->suburb->name.' - '.$job->suburb->state->name}}</p>
+            <p>{{'$'.$job->annual_salary}} per month</p>
+            <p>{{'Role: '.$job->subClassification->name}}</p>
+            <p class="text-gray-400 mt-3 mb-3">{{ $job->description }}</p>
+            <span class="">☆</span>
+            <span class="text-blue-600 text-xl link-hover cursor-pointer">Save</span>
+            <hr class="mt-5 border-t-2">
+        </h3>
+          @endforeach
+            <button class="flex justify-center text-white rounded-md">More...</p>
+        @endif  
+      </div>
     </div>
 </div>
 
