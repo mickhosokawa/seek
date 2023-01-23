9f65c5ae4bc5c936c4871c78a133c8a5d838f1ec
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-01-24 06:48:44 +0900

    お気に入り登録関連のファイルを準備

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/career.js') }}"></script>
+        <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         
     </head>
